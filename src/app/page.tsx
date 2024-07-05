aa1f6add9c264b4e2bea65b8560129b39abbc529
MindbniM <1771392560@qq.com>
2024-07-05 15:07:11 +0800

    修正小图标

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,34 @@
+import Head from 'next/head';
+
 export default async function Home() {
   return (
-    <main>
-      <section className="home section" id="home">
-        <div className="container">
-          <div className="intro">
-            <img
-              src="/imgs/MindbniM.png"
-              alt="Al Siam Profile"
-              className="shadow-dark"
-            />
-            <h1>MindbniM</h1>
-            <div className="social-links">
-              <a href="https://github.com/MindbniM" target="_blank">
-                <i className="fa fa-github" />
-              </a>
-              <a href="https://blog.csdn.net/2301_77838258?spm=1000.2115.3001.5343" target="_blank">
-                <i className="fa fa-rss" />
-              </a>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Your Website Title</title>
+      </Head>
+      <main>
+        <section className="home section" id="home">
+          <div className="container">
+            <div className="intro">
+              <img
+                src="/imgs/MindbniM.png"
+                alt="Profile"
+                className="shadow-dark"
+              />
+              <h1>MindbniM</h1>
+              <div className="social-links">
+                <a href="https://github.com/MindbniM" target="_blank" rel="noopener noreferrer">
+                  <i className="fa fa-github" />
+                </a>
+                <a href="https://blog.csdn.net/2301_77838258?spm=1000.2115.3001.5343" target="_blank" rel="noopener noreferrer">
+                  <i className="fa fa-rss" />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   );
 }
